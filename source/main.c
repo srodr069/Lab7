dfa8a867f096eb2d6060b548e59f6b6e7591549c
StevenRodriguez <srodr069@ucr.edu>
2020-01-30 02:41:34 -0800

    board success

--- source/main.c
+++ source/main.c
@@ -8,15 +8,24 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "io.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+
+    LCD_init();
+
+    LCD_DisplayString(1, "Reeeeeeeeeee...");
+
 
     /* Insert your solution below */
-    while (1) {
+    while (1) { 
+    	continue;
 
     }
     return 1;
